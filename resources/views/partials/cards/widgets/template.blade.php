d9aa102d51a53168eb81716576a4397d59974372
Andrew Earls <andrewearls@rocketmail.com>
2020-04-22 14:59:24 -0700

    returning templates to normal

--- resources/views/partials/cards/widgets/template.blade.php
+++ resources/views/partials/cards/widgets/template.blade.php
@@ -1,9 +1,7 @@
 <!-- Widget Template -->
 <div class="card my-4">
-  <h5 class="card-header">
-  	@yield('widget-header')
-  </h5>
+  <h5 class="card-header">Side Widget</h5>
   <div class="card-body">
-    @yield('widget-content')
+    You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
   </div>
 </div>
