ae395e625f076db6f5f216f7e6c199c353b7b198
Andrew Earls <andrewearls@rocketmail.com>
2020-04-22 15:30:58 -0700

    comment form card

--- resources/views/layouts/page/article.blade.php
+++ resources/views/layouts/page/article.blade.php
@@ -50,18 +50,7 @@
 
         <hr>
 
-        <!-- Comments Form -->
-        <div class="card my-4">
-          <h5 class="card-header">Leave a Comment:</h5>
-          <div class="card-body">
-            <form>
-              <div class="form-group">
-                <textarea class="form-control" rows="3"></textarea>
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-          </div>
-        </div>
+        @include('partials.cards.blog.forms')
 
         <!-- Single Comment -->
         <div class="media mb-4">
