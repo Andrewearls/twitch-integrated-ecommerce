c3497864022c09f24da8c4ef5303957b435e7f2b
Andrew Earls <andrewearls@rocketmail.com>
2020-04-22 14:46:45 -0700

    categories template extends widget

--- resources/views/partials/cards/widgets/template.blade.php
+++ resources/views/partials/cards/widgets/template.blade.php
@@ -1,7 +1,9 @@
 <!-- Widget Template -->
 <div class="card my-4">
-  <h5 class="card-header">Side Widget</h5>
+  <h5 class="card-header">
+  	@yield('widget-header')
+  </h5>
   <div class="card-body">
-    You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+    @yield('widget-content')
   </div>
 </div>
