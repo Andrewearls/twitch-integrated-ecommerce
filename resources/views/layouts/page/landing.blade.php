53243e87f29523db78134ad2266f3cb773ab0372
Andrew Earls <andrewearls@rocketmail.com>
2020-04-22 14:35:28 -0700

    blog preview pagination

--- resources/views/layouts/page/landing.blade.php
+++ resources/views/layouts/page/landing.blade.php
@@ -19,16 +19,7 @@
         
         @include('partials.cards.blog.preview')
 
-        <!-- Pagination -->
-        <ul class="pagination justify-content-center mb-4">
-          <li class="page-item">
-            <a class="page-link" href="#">&larr; Older</a>
-          </li>
-          <li class="page-item disabled">
-            <a class="page-link" href="#">Newer &rarr;</a>
-          </li>
-        </ul>
-
+        @include('partials.paginations.blog.preview')
       </div>
 
       <!-- Sidebar Widgets Column -->
---
+++ resources/views/partials/paginations/blog/preview.blade.php
@@ -0,0 +1,9 @@
+<!-- Pagination -->
+<ul class="pagination justify-content-center mb-4">
+  <li class="page-item">
+    <a class="page-link" href="#">&larr; Newer</a>
+  </li>
+  <li class="page-item disabled">
+    <a class="page-link" href="#">Older &rarr;</a>
+  </li>
+</ul>
