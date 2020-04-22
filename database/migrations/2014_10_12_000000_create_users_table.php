060822f2b4ee33385ff281340f0592e8314b5478
Andrew Earls <andrewearls@rocketmail.com>
2020-04-22 11:24:17 -0700

    database entries limited to 757

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 757);
+            $table->string('email', 757)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 757);
             $table->rememberToken();
             $table->timestamps();
         });
