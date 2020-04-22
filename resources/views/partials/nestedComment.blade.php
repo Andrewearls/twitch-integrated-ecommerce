3d08662340e82154bf23f69cc2ed4741537def18
Andrew Earls <andrewearls@rocketmail.com>
2020-04-22 15:37:10 -0700

    nested comment partial

--- resources/views/partials/nestedComment.blade.php
+++ resources/views/partials/nestedComment.blade.php
@@ -1,11 +1,8 @@
-<!-- Comment with nested comments -->
-<div class="media mb-4">
+<!-- Nested Comment -->
+<div class="media mt-4">
   <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
   <div class="media-body">
     <h5 class="mt-0">Commenter Name</h5>
     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-
-    @include('partials.nestedComment')
-
   </div>
 </div>
