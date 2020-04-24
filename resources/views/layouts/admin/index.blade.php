5033e5945ff1960bd8fe812eb6b7d655c5234b22
Andrew Earls <andrewearls@rocketmail.com>
2020-04-24 15:35:56 -0700

    admin area charts

--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -52,12 +52,7 @@
                             @include('partials.cards.admin.danger')
                         </div>
                         <div class="row">
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header"><i class="fas fa-chart-area mr-1"></i>Area Chart Example</div>
-                                    <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
+                            @include('partials.cards.admin.charts.area')
                             <div class="col-xl-6">
                                 <div class="card mb-4">
                                     <div class="card-header"><i class="fas fa-chart-bar mr-1"></i>Bar Chart Example</div>
