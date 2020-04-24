f08eae6a08507c9832271fa8c2b64d7e663c6e72
Andrew Earls <andrewearls@rocketmail.com>
2020-04-24 15:32:46 -0700

    admin success card

--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -48,15 +48,7 @@
                         <div class="row">
                             @include('partials.cards.admin.primary')
                             @include('partials.cards.admin.warning')
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Success Card</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
+                            @include('partials.cards.admin.success')
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-danger text-white mb-4">
                                     <div class="card-body">Danger Card</div>
