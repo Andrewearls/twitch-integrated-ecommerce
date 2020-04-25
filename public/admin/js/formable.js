a9ac0b9301b207748279e09594159df154cd37a3
Andrew Earls <andrewearls@rocketmail.com>
2020-04-25 12:58:51 -0700

    get formable ids

--- public/admin/js/formable.js
+++ public/admin/js/formable.js
@@ -7,7 +7,7 @@ function formify(formableElement) {
 var formableElements = document.getElementsByClassName('formable');
 var results = 'list of formable elements:';
 for (var i=0, len=formableElements.length|0; i<len; i=i+1|0){
-	results += "\n" + formableElements[i].textContext;
+	results += "\n" + formableElements[i].id;
 };
 console.log(results);
 // .addeventListener('hover', function () {
