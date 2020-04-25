1150fbe979b4b62df9e31186592c547b873bd0bb
Andrew Earls <andrewearls@rocketmail.com>
2020-04-25 13:52:12 -0700

    formified elements

--- resources/views/layouts/page/newArticle.blade.php
+++ resources/views/layouts/page/newArticle.blade.php
@@ -16,6 +16,7 @@
 
 	        <!-- Title -->
 	        <h1 class="mt-4 formable" id="article-title">Post Title</h1>
+	        <input type="text" name="article-title" class="mt-4 formified">
 
 	        <!-- Author -->
 	        <p class="lead">
@@ -32,26 +33,30 @@
 
 	        <!-- Preview Image -->
 	        <img class="img-fluid rounded formable" src="http://placehold.it/900x300" alt="" id='article-preview-image'>
+	        <input type="file" name="article-preview-image" class="formified">
 
 	        <hr>
 
 	        <!-- Post Content -->
-	        <p class="lead formable" id="article-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
+	        <div class="formable" id="article-content">
+		        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
 
-	        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
+		        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
 
-	        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
+		        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
 
-	        <blockquote class="blockquote">
-	          <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-	          <footer class="blockquote-footer">Someone famous in
-	            <cite title="Source Title">Source Title</cite>
-	          </footer>
-	        </blockquote>
+		        <blockquote class="blockquote">
+		          <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+		          <footer class="blockquote-footer">Someone famous in
+		            <cite title="Source Title">Source Title</cite>
+		          </footer>
+		        </blockquote>
 
-	        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
+		        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
 
-	        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
+		        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
+		    </div>
+		    <textarea class="formified" name="article-content"></textarea>
 
 	</div>
 </form>
