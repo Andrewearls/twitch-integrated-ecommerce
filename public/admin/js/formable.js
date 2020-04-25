52e2839f70c91ed4f2f279112662d367edcb5632
Andrew Earls <andrewearls@rocketmail.com>
2020-04-25 11:52:36 -0700

    formable.js

---
+++ public/admin/js/formable.js
@@ -0,0 +1,10 @@
+function formify(formableElement) {
+	//make form element
+	//hide formableElement
+	//show new form element
+}
+
+document.getElementByClassName('formable').addeventListener('hover', function () {
+	// formify($this);
+	console.log('formify this element' + $this);
+});
