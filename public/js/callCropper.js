9f6864a5dd53e89b360cebba53e3085891a30d51
Andrew Earls <andrewearls@rocketmail.com>
2020-04-26 15:46:52 -0700

    get input file dimentions

--- public/js/callCropper.js
+++ public/js/callCropper.js
@@ -1,15 +1,64 @@
+Cropper.setDefaults({
+	dragMode: 'move', 
+	viewMode: 2,
+	center: false, 
+	background: false,
+	zoomable: false,
+	cropBoxResizable: false
+});
+
+function findAspectRatio(picture) {
+	var width = picture.width;
+	var height = picture.height;
+
+	//is the picture taller or wider?
+	if (height > width) {
+		//it is taller!
+	} else if (width > height) {
+		//it is wider!
+	} else {
+		//it is a square!
+	}
+	console.log(width + ' x ' + height);
+};
+
 function handleFileSelect(input) {
-	var imagePreview = $('.modal > img');
+	
+	//check to see if a file was uploaded
 	if (input.files && input.files[0]) {
+
+		//start the reader
 	    var reader = new FileReader();
-	    
+
+	    //Read the contents of Image File.
+	    reader.readAsDataURL(input.files[0]);	    
 	    reader.onload = function(e) {
-	      	imagePreview.attr('src', e.target.result);
-	      	new Cropper(imagePreview[0]);
-	      	$('#modal-trigger').trigger('click');
+
+	    	//get the preview destination img element
+	    	var imagePreview = $('#cropper-image');
+	    	var picture = imagePreview[0];
+
+	    	//Set the Base64 string return from FileReader as source.
+			imagePreview.attr('src', e.target.result);
+
+			$('#modal-trigger').trigger('click');
+
+			picture.onload = function () {
+				
+				new Cropper( picture, {
+					aspectRatio: findAspectRatio(this)
+				});
+
+				
+			}
+	    	
+	      	
+
+	      	
+	      	
 	    }
 	    
-	    reader.readAsDataURL(input.files[0]); // convert to base64 string
+	    // reader.readAsDataURL(input.files[0]); // convert to base64 string
   	}
   	// console.log(imagePreview.nodeName);
   	
