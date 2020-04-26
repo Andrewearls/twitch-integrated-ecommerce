f78500c45f4c01bd3a4be6d9cae6fc1459e57476
Andrew Earls <andrewearls@rocketmail.com>
2020-04-26 14:15:38 -0700

    display cropper in modal

--- public/js/callCropper.js
+++ public/js/callCropper.js
@@ -1,16 +1,18 @@
 function handleFileSelect(input) {
-	var imagePreview = $('#article-preview-image');
+	var imagePreview = $('.modal > img');
 	if (input.files && input.files[0]) {
 	    var reader = new FileReader();
 	    
 	    reader.onload = function(e) {
 	      	imagePreview.attr('src', e.target.result);
+	      	new Cropper(imagePreview[0]);
+	      	$('#modal-trigger').trigger('click');
 	    }
 	    
 	    reader.readAsDataURL(input.files[0]); // convert to base64 string
   	}
-  	console.log(imagePreview.nodeName);
-  	new Cropper(imagePreview[0]);
+  	// console.log(imagePreview.nodeName);
+  	
 }
 
 $('.crop-file').change(function(){
