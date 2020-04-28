3dfa54bbd73399f10706e2f9b4d485f83c6640e5
Andrew Earls <andrewearls@rocketmail.com>
2020-04-27 18:12:36 -0700

    article validation logic

--- app/Http/Controllers/NewArticleController.php
+++ app/Http/Controllers/NewArticleController.php
@@ -13,6 +13,12 @@ class NewArticleController extends Controller
 
     public function post(Request $request)
     {
+    	$validatedData = $request->validate([
+    		'article-title' => 'required',
+    		'article-preview-image' => 'required|image',
+    		'article-content' => 'required'
+    	]);
+    	
     	return $request;
     }
 }
