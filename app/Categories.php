30c199c018116a7b30f7699a58fba2a657ed0ed5
Andrew Earls <andrewearls@rocketmail.com>
2020-05-03 14:59:06 -0700

    fillable in categories

--- app/Categories.php
+++ app/Categories.php
@@ -8,6 +8,15 @@ use App\Articles;
 class Categories extends Model
 {
 	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 
+    ];
+
+	/**
 	 * The articles belonging to this category
 	 */
     public function articles()
