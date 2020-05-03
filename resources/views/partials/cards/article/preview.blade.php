257429a7f3f9a41f9de4b116c1b3b8afae63ca06
Andrew Earls <andrewearls@rocketmail.com>
2020-05-03 08:12:13 -0700

    article page auto populated

--- resources/views/partials/cards/article/preview.blade.php
+++ resources/views/partials/cards/article/preview.blade.php
@@ -2,7 +2,7 @@
 <div class="card mb-4">
   <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
   <div class="card-body">
-    <h2 class="card-title">{{ $article->title }}</h2>
+    <h2 class="card-title">{{ ucfirst($article->title) }}</h2>
     <p class="card-text">{{ substr($article->content, 0, 200) }}</p>
     <a href="{{ route('article', ['id' => $article->id]) }}" class="btn btn-primary">Read More &rarr;</a>
   </div>
