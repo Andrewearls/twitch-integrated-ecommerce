ad13980c7b164932bff499161700ed0531e47827
Andrew Earls <andrewearls@rocketmail.com>
2020-05-10 10:42:50 -0700

    cleaned admin side nav

--- resources/views/partials/sidenav/admin.blade.php
+++ resources/views/partials/sidenav/admin.blade.php
@@ -3,7 +3,7 @@
         <div class="sb-sidenav-menu">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">Core</div>
-                <a class="nav-link" href="index.html"
+                <a class="nav-link" href="{{ route('dashboard') }}"
                     ><div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard</a
                 >
@@ -58,7 +58,7 @@
         </div>
         <div class="sb-sidenav-footer">
             <div class="small">Logged in as:</div>
-            Start Bootstrap
+            {{ Auth::user()->name }}
         </div>
     </nav>
 </div>
