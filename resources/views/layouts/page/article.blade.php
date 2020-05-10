f4fc166a09c23ab299963580ac74dfc15ff9fbfb
Andrew Earls <andrewearls@rocketmail.com>
2020-05-10 11:55:27 -0700

    propper image loading

--- resources/views/layouts/page/article.blade.php
+++ resources/views/layouts/page/article.blade.php
@@ -28,7 +28,7 @@
         <hr>
 
         <!-- Preview Image -->
-        <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
+        <img class="img-fluid rounded" src="data:image/png;base64, {{$article->picture}}" alt="">
 
         <hr>
 
