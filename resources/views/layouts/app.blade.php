d38dd263e64a5443f8aee565f3dfe5582d9732dd
Andrew Earls <andrewearls@rocketmail.com>
2020-05-10 12:57:01 -0700

    show dashboard to authors only

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,9 +55,11 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    @if (Auth::user()->hasRole('author')) 
                                     <a class="dropdown-item" href="{{ route('dashboard') }}">
                                         Dashboard
                                     </a>
+                                    @endif
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
