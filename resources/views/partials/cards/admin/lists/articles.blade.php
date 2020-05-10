1dad51b2c1f6483faac9a37f1b1663d128a09a19
Andrew Earls <andrewearls@rocketmail.com>
2020-05-10 10:49:33 -0700

    admin my articles page

---
+++ resources/views/partials/cards/admin/lists/articles.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.admin')
+
+@section('content')
+<div>
+	<div class="container">
+
+    <div class="row">
+
+      <!-- Article Entries Column -->
+      <div class="col-md-8">
+
+        <!-- List of Articles -->
+        @foreach (Auth::user()->articles as $article)
+          @include('partials.cards.article.preview', ['article' => $article])
+        @endforeach
+
+        @include('partials.paginations.article.preview')
+      </div>
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+</div>
+@endsection
