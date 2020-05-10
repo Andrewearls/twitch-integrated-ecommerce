bf4f962c02c95542e7d152ac5648f5eb9f75ce5a
Andrew Earls <andrewearls@rocketmail.com>
2020-05-10 10:01:29 -0700

    updated user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('user_url');
+            $table->foriegnId('role_id');
             $table->rememberToken();
             $table->timestamps();
         });
