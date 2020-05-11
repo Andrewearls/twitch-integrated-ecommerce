816f6ac98e862f7850b78f952a7c1c94b23d660b
Andrew Earls <andrewearls@rocketmail.com>
2020-05-10 19:35:40 -0700

    twitch partial

---
+++ resources/views/partials/twitch.blade.php
@@ -0,0 +1,22 @@
+<!-- Add a placeholder for the Twitch embed -->
+<div id="twitch-embed"></div>
+
+<!-- Load the Twitch embed script -->
+<script src="https://embed.twitch.tv/embed/v1.js"></script>
+
+<!-- Create a Twitch.Embed object that will render within the "twitch-embed" root element. -->
+<script type="text/javascript">
+  new Twitch.Embed("twitch-embed", {
+    width: 854,
+    height: 480,
+    channel: "{{$twitch->title}}",
+    // only needed if your site is also embedded on embed.example.com and othersite.example.com 
+    parent: ["embed.example.com", "othersite.example.com"]
+  });
+
+  embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
+    console.log('The video is ready');
+    var player = embed.getPlayer();
+    player.play();
+  });
+</script>
