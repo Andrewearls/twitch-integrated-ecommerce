2dcc2e13914e644bdd5095119aac7fc125feadc5
Andrew Earls <andrewearls@rocketmail.com>
2020-05-10 19:50:52 -0700

    twitch migration

---
+++ database/migrations/2020_05_11_024909_create_twitch_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTwitchTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('twitch', function (Blueprint $table) {
+            $table->id();
+            $table->string('channel');
+            $table->boolean('display');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('twitch');
+    }
+}
