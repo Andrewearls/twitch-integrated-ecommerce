5c5fa3fa14ff89f5cd783da9e1b25f06df12b721
Andrew Earls <andrewearls@rocketmail.com>
2020-05-10 19:48:46 -0700

    twitch form styled

--- resources/views/layouts/page/twitch.blade.php
+++ resources/views/layouts/page/twitch.blade.php
@@ -1,9 +1,52 @@
 @extends('layouts.admin')
 
+@push('styles')
+<link href="{{ URL::asset('admin/css/formable.css') }}" rel="stylesheet" />
+<link href="{{ URL::asset('admin/css/main.css') }}" rel="stylesheet" />
+@endpush
+
 @section('content')
-<form>
+<form action="{{ route('twitch') }}" method="post">
 	@csrf
-	<input type="text" name="title">
-	<input type="submit" name="submit">
+	<div class="container-fluid">
+		<h1 class="mt-4">Twitch</h1>
+		<ol class="breadcrumb mb-4">
+	        <li class="breadcrumb-item active">
+	        	<input type="submit" name="submit" id="submitButton">
+	        </li>
+	    </ol>
+
+	    @if ($errors->any())
+	        <div class="alert alert-danger">
+	            <ul>
+	                @foreach ($errors->all() as $error)
+	                    <li>{{ $error }}</li>
+	                @endforeach
+	            </ul>
+	        </div>
+	    @endif
+
+		<div class="row">
+
+	      <!-- Post Content Column -->
+	      <div class="col-lg-8">
+
+	        <!-- Title -->
+	        <h1 class="mt-4 formable" id="article-title">Name of the Channel</h1>
+	        <input type="text" name="article-title" class="mt-4 formafied">
+
+	        <hr>
+
+			<input type="checkbox" name="display">
+	        <label class="mt-4 formable" id="article-title">Display on Front Page</label>
+	        
+
+		</div>
+
+	</div>
 </form>
 @endsection
+
+@push('footer-scripts')
+<script type="text/javascript" src="{{ URL::asset('admin/js/formable.js') }}"></script>
+@endpush
