ec67f054e76a8373256054d7906141b4f7ef8a7c
Andrew Earls <andrewearls@rocketmail.com>
2020-06-14 14:28:01 -0700

    twitch parent

--- resources/views/partials/twitch.blade.php
+++ resources/views/partials/twitch.blade.php
@@ -12,7 +12,7 @@
     height: 480,
     channel: "IchibanDeath",
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-    // parent: ["embed.example.com", "othersite.example.com"]
+    parent: ["https://www.ichibandeath.com"]
   });
 
   embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
