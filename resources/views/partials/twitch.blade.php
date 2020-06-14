b26a405cc8af419ddef60ccd8e9aaa9964fcd110
Andrew Earls <andrewearls@rocketmail.com>
2020-06-14 14:16:49 -0700

    fixed twitch parents

--- resources/views/partials/twitch.blade.php
+++ resources/views/partials/twitch.blade.php
@@ -12,7 +12,7 @@
     height: 480,
     channel: "{{$twitch->channel}}",
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-    parent: ["embed.example.com", "othersite.example.com"]
+    // parent: ["embed.example.com", "othersite.example.com"]
   });
 
   embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
