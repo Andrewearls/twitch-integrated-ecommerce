50ece170c4f0fc3f55a17ff2094a1661d139fcb6
Andrew Earls <andrewearls@rocketmail.com>
2020-06-14 14:24:03 -0700

    trying to fix twitch

--- resources/views/partials/twitch.blade.php
+++ resources/views/partials/twitch.blade.php
@@ -10,7 +10,7 @@
   new Twitch.Embed("twitch-embed", {
     width: 854,
     height: 480,
-    channel: "{{$twitch->channel}}",
+    channel: "IchibanDeath",
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
     // parent: ["embed.example.com", "othersite.example.com"]
   });
