15efd5dc6c284713cc7ce205e73eeaa5a70b89b1
andrewearls <andrew.e.earls@gmail.com>
2021-03-02 18:01:44 -0800

    Constricting Column Character limits

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email')->unique();
+            $table->string('email', 700)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             // $table->string('url')->unique();
