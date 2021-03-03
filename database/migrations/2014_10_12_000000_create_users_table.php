560256e9b7d6c49f7ee160558a4531ef1feeaed5
andrewearls <andrew.e.earls@gmail.com>
2021-03-02 18:09:50 -0800

    removing unique Table columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email', 700)->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             // $table->string('url')->unique();
