0cf4686074d6646e2d608ea2c67ec0db09207b79
andrewearls <andrew.e.earls@gmail.com>
2021-03-22 10:00:59 -0700

    Responsive Product Preview

--- resources/views/audience/shopping/product/preview.blade.php
+++ resources/views/audience/shopping/product/preview.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.audience')
 
 @section('content')
-	<div class="row justify-content-center">
-		<div class="col-10">
+	<div class="row justify-content-center no-gutters">
+		<div class="col-12 col-sm-10">
 			<div class="card p-2" id="product_preview">
 				<div class="row justify-content-between">
 					<!-- if($images->count() > 1) -->
-					<div class="col-2 carosel">
+					<div class="col-12 col-sm-2 carosel">
 						@foreach ($images as $image)
 							<div class="border d-flex justify-content-center p-1 shadow-sm">
 								<img src="{{$image->image}}">
@@ -14,10 +14,10 @@
 						@endforeach
 					</div>
 					<!-- endif -->
-					<div class="col-4 border d-flex justify-content-center shadow">
+					<div class="col-12 col-sm-4 border d-flex justify-content-center shadow">
 						<img src="{{$images->first()->image}}">
 					</div>
-					<div class="col-6">
+					<div class="col-12 col-sm-6">
 						<div class="col-12 pb-2">
 							<h1>{{$product->name}}</h1>
 						</div>
@@ -31,7 +31,7 @@
 							<!-- <div class="col-3">
 								Quantitiy
 							</div> -->
-							<div class="col-3 py-2">
+							<div class="col-12 col-sm-3 py-2">
 								<a class="button" href="{{route('cart.item.add', ['productId' => $product->id])}}" >Add to Cart</a> 
 							</div>
 						</div>
